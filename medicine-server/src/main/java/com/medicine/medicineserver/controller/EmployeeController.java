0d8820a32dfb25f176529d76d02ce6f9c2e30b95
dhw-ccy <52777310+dhw-ccy@users.noreply.github.com>
2020-03-10 16:12:44 +0800

    Update EmployeeController.java

--- medicine-server/src/main/java/com/medicine/medicineserver/controller/EmployeeController.java
+++ medicine-server/src/main/java/com/medicine/medicineserver/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//提交
 @RestController
 public class EmployeeController {
 
